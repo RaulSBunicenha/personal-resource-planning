bd7041cb431535977f1fd64f94e362adf30d9a49
Raul B <raulbunicenha@gmail.com>
2021-04-29 17:28:59 -0300

    Test Postgres Connection

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const { createServer } = require('http')
-const Sequelize = require('sequelize')
+const { Sequelize, DataTypes } = require('sequelize')
 const port = process.env.PORT || 3000
 
-const database = new Sequelize({
+function createDatabase () {
+  const database = new Sequelize({
     dialect: 'postgres',
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
@@ -11,11 +12,65 @@ const database = new Sequelize({
     password: process.env.DB_PASSWORD,
     logging: false,
     define: {
-        underscored: true,
+      underscored: true,
     }
-})
+  })
+
+  return database
+}
+
+async function tryConnection (database) {
+  await database.authenticate()
+  return true
+}
+
+async function defineUserModel (database) {
+  const User = database.define('User', {
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    lastName: {
+      type: DataTypes.STRING
+    }
+  })
+
+  await User.sync({ force: true })
+
+  return User
+}
+
+async function addUser (User, ...{ firstName, lastName }) {
+  const user = User.build({ firstName, lastName })
+  await user.save()
+  return user
+}
+
+async function listUser (User) {
+  return User.find()
+}
 
 createServer(async (req, res) => {
-    res.writeHeader('200')
-    res.end('Hello World')
+  try {
+    const database = createDatabase()
+    const User = await defineUserModel(database)
+    await addUser(User, 'Raul', 'Bunicenha')
+    await addUser(User, 'Renata', 'Longhi')
+
+    const userList = await listUser(User)
+
+    res.writeHead(200)
+    res.end(JSON.stringify({
+      status: true,
+      data: userList
+    }))
+  } catch (err) {
+    res.writeHead(500)
+    res.end(JSON.stringify({
+      status: false,
+      error: {
+        message: err.message
+      }
+    }))
+  }
 }).listen(port, () => console.log(`Server started on port ${port}`))
